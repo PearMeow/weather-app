eaaf5b642b06c450a9f8d9ff0ac124667125a9e9
Perry Huang <perryhuang2015@gmail.com>
2025-07-31 17:42:29 -0400

    Add function to get information from api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 console.log("This is some template text");
+
+const key = "";
+
+async function foo() {
+    const data = await fetch(
+        "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/New%20York?unitGroup=us&key=" +
+            key +
+            "&contentType=json",
+    );
+    console.log(data);
+}
+
+foo();
