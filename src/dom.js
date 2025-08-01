98335a31051783fc2d9be9587c5991747b858886
Perry Huang <perryhuang2015@gmail.com>
2025-08-01 12:44:13 -0400

    Change formatting of info and position input to top

--- src/dom.js
+++ src/dom.js
@@ -2,17 +2,21 @@ console.log("This is the dom module");
 
 const info = document.querySelector(".info");
 
-export function displayInfo(address, temp, conditions, desc) {
-    const newPara = document.createElement("p");
-    newPara.textContent =
-        address +
-        ", Temperature: " +
-        temp +
-        " degrees Fahrenheit" +
-        ", Conditions: " +
-        conditions +
-        " " +
-        desc;
+export function displayInfo(address, temperature, conditions, description) {
+    const infoList = document.createElement("ul");
+    const addrItem = document.createElement("li");
+    const tempItem = document.createElement("li");
+    const conditionItem = document.createElement("li");
+    const descItem = document.createElement("li");
+    addrItem.textContent = address;
+    tempItem.textContent =
+        "Temperature: " + temperature + " degress Fahrenheit";
+    conditionItem.textContent = "Conditions: " + conditions;
+    descItem.textContent = description;
     info.replaceChildren();
-    info.appendChild(newPara);
+    infoList.appendChild(addrItem);
+    infoList.appendChild(tempItem);
+    infoList.appendChild(conditionItem);
+    infoList.appendChild(descItem);
+    info.appendChild(infoList);
 }
